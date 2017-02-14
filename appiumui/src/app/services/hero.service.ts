38969c41ff4a8d1ad826639fe640f299eb487220
niteshbisht <niteshsingh98@outlook.com>
2017-02-15 04:11:46 +0530

    completed implementations typescript

--- appiumui/src/app/services/hero.service.ts
+++ appiumui/src/app/services/hero.service.ts
@@ -1,7 +1,7 @@
 //hero.service.ts
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from '../common/hero';
+import { HEROES } from '../common/mock-heroes';
 
 @Injectable()
 export class HeroService {
